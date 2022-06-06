42cc2a25f71963ec3fee99d8647741d38c141ba4
View <thanathip804@gmail.com>
2022-06-06 10:14:20 +0700

    create line-liff-auto-login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import liff from '@line/liff';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Frontend-Angular-Tailwind';
 }
+
+liff.init({
+  liffId: '1657193195-wMZjNpWK', // Use own liffId
+  withLoginOnExternalBrowser: true, // Enable automatic login process
+}).then(() => {
+  // Start to use liff's api
+});
